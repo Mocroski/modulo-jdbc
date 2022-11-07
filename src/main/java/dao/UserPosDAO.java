2b0d54cc69a97d69014f7ab0be0fe3cadcd4ef54
Lucas Mocroski <devmocroski@gmail.com>
2022-11-07 10:18:17 -0300

    criando estrutura de DAO de acesso ao banco de dados

---
+++ src/main/java/dao/UserPosDAO.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.sql.Connection;
+
+import conexaojdbc.SingleConnection;
+
+public class UserPosDAO {
+	
+	private Connection connection;
+	
+	public  UserPosDAO() {
+		connection = SingleConnection.getConnection();
+		
+	}
+
+}
