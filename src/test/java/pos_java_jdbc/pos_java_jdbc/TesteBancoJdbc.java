27e0aef4edfef150a388dbcde52f75eed48c19bc
Lucas Mocroski <devmocroski@gmail.com>
2022-11-07 11:19:00 -0300

    Criando INSERT ao banco de dados

--- src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -3,11 +3,22 @@ package pos_java_jdbc.pos_java_jdbc;
 import org.junit.Test;
 
 import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteBancoJdbc {
 
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		
+		userposjava.setId(5L);
+		userposjava.setNome("teste 2");
+		userposjava.setEmail("teste2@gmail.com");
+		
+		userPosDAO.salvar(userposjava);
+		
 	}
 }
